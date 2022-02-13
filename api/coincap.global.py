b48e22b7a2f5c9fdd470c919cd748329991cedde
John Trujillo <amriikk@gmail.com>
2022-02-13 04:20:02 -0800

    fix api call typos

--- api/coincap.global.py
+++ api/coincap.global.py
@@ -2,13 +2,13 @@ import requests
 import json
 
 api_key = 'e839c988-6481-4eff-a380-8f898e98a298'
-headers {'X-CMC_PRO_API_KEY': api_key}
+headers = {'X-CMC_PRO_API_KEY': api_key}
 
 base_url = 'https://pro-api.coinmarketcap.com'
 
 global_url = base_url + '/v1/global-metrics/quotes/latest'
 
 request = requests.get(global_url, headers=headers)
-results = requests.json()
+results = request.json()
 
-print(json.dumps(results, sort_key=True, indent=4))
+print(json.dumps(results, sort_keys=True, indent=4))
